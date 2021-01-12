dcff8de541706363264802c0d36e1ccd478ddf57
Harry Yuan <harryuan.65@gmail.com>
2021-01-12 10:25:50 +0800

    feat: add before_action authenticate_user

--- app/controllers/video_watching_histories_controller.rb
+++ app/controllers/video_watching_histories_controller.rb
@@ -1,4 +1,5 @@
 class VideoWatchingHistoriesController < ApplicationController
+  before_action :authenticate_user!
   def my_histories
     @result = UserVideoReport.where(user_id: current_user.id)
     render json: @result
