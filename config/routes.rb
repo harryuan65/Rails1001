050ac7b7b988bd5aafd1782861119caf8424c2b1
harryuan65 <harryuan.65@gmail.com>
2022-01-06 15:49:13 +0800

    feat: engine#1 mount

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount CheeseApi::Engine, at: '/api/cheese'
 end
