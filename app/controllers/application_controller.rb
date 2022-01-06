5b9bdedb548ad0035dda088ba66562d10306df0f
harryuan65 <harryuan.65@gmail.com>
2022-01-06 16:27:23 +0800

    feat: add test post route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def test
+    res = params.permit(*params.keys).to_h.each_with_object([]) do |(k, v), a|
+      a << "#{k}: #{v}, type is #{v.class}"
+    end
+    render json:res
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       get :summary2
     end
   end
+  post '/test', to: 'application#test'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount CheeseApi::Engine, at: '/api/cheese'
 end
